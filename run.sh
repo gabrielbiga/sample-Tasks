e96cd6bd917e1a4b7a939bb036b491c0e502b8e7
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-05-07 17:40:34 +0300

    Add the automation code

---
+++ run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+gitresult=$( { git status | grep "fatal: Not a git repository" > /dev/null; } 2>&1 )
+if [[ ! -d "./sample-Tasks" && "${gitresult}" != "" ]]; then
+# Cloning to default dir
+    echo "=> Downloading sample-Tasks from git"
+    printf "\r=> "
+    command git clone "git@github.com:NativeScript/sample-Tasks.git"
+    cd sample-Tasks
+fi
+
+npmresult=$(npm list -g | grep 'nativescript@');
+if [[ "$npmresult" == "" ]]; then
+    npm install -g nativescript
+fi
+
+unamestr=`uname`
+if [[ "$unamestr" == "Darwin" ]]; then
+    iosresult=$(tns platform list | grep 'Installed platforms.*ios');
+    if [[ "$iosresult" == "" ]]; then
+        tns platform add ios
+    fi
+    tns run ios --emulator
+else
+    androidresult=$(tns platform list | grep 'Installed platforms.*android');
+    if [[ "$androidresult" == "" ]]; then
+        tns platform add android
+    fi
+    tns run android --emulator
+fi
+
